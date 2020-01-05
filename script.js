a806d24eae543adae9a114f482eb4c4b201bee54
Sebastian <37258153+PenguinDesign@users.noreply.github.com>
2020-01-04 21:19:12 -0600

    updage script and navigation

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
 let navigationLinkList = document.getElementsByClassName('navigation__link');
 let navigationCheckBox = document.querySelector('input');
 
+let disableAllLinks = true;
+
+for (let i = 0; i < navigationLinkList.length; i++) {
+    navigationLinkList[i].style.pointerEvents = 'none';
+}
+
+navigationCheckBox.addEventListener('click', function () {
+    disableAllLinks = !disableAllLinks;
+    console.log(disableAllLinks);
+    for (let i = 0; i < navigationLinkList.length; i++) {
+        if (disableAllLinks) {
+            navigationLinkList[i].style.pointerEvents = 'none';
+        } else {
+            navigationLinkList[i].style.pointerEvents = 'auto';
+        }
+    }
+})
+
 for (let i = 0; i < navigationLinkList.length; i++) {
     navigationLinkList[i].addEventListener('click', function () {
         navigationCheckBox.checked = false;
+        for (let i = 0; i < navigationLinkList.length; i++) {
+            disableAllLinks = true;
+            navigationLinkList[i].style.pointerEvents = 'none';
+        }
     })
 }
